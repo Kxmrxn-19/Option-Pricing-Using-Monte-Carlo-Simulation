a7831b474860b103c1b44dd37eb4068b59ae5998
Kamran_Mehdi <89677300+Kxmrxn-19@users.noreply.github.com>
2025-11-30 15:50:50 +0530

    Update BSM_option_class.py

--- black_scholes_merton/BSM_option_class.py
+++ black_scholes_merton/BSM_option_class.py
@@ -1,9 +1,6 @@
 # -*- coding:utf-8 -*-
 #######################################################################
-# Copyright (C) 2016 Shijie Huang (harveyh@student.unimelb.edu.au)    #
-# Permission given to modify the code as long as you keep this        #
-# declaration at the top                                              #
-#######################################################################
+
 from utils import barrier_warning
 import numpy as np
 from numpy import log, exp, sqrt
